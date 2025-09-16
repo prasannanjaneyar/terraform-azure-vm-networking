3300f74d0381070397c55b7f13e2a06d519d82c5
Prasannanjaneya <Prasannanjaneya.r@gmail.com>
2025-09-16 14:38:01 +0200

    added new file backend

--- provider.tf
+++ provider.tf
@@ -11,3 +11,11 @@ terraform {
 provider "azurerm" {
   features {}
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorage234"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
